5516169a44be06b0dcf34f044df39b6b615ed91b
stvenduong <stvenduong@gmail.com>
2020-12-29 18:21:59 -0800

    Fixed minor proptype error in Ratings component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -43,10 +43,12 @@ const ProductScreen = ({ history, match }) => {
                 <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                />
+                {product.rating && (
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
+                )}
               </ListGroup.Item>
               <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
               <ListGroup.Item>
